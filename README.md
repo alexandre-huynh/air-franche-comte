defb2ea06320a21907ef4297c6725425d3ed3dd9
HUYNH Alexandre <huynh@et.esiea.fr>
2025-07-17 10:19:36 -0400

    Reorganized in backend & frontend

--- README.md
+++ README.md
@@ -2,12 +2,26 @@
 
 Init with this
 ```bash
+cd backend
+npm install
+
+cd ../frontend
 npm install
 ```
 
-Create a .env and fill variables, refer to .env_example
-
-Run app
+Create a .env in backend and fill variables, refer to .env_example
 ```bash
-node index.js
+cd backend
+```
+
+Run back in 1st terminal
+```bash
+cd backend
+node server.js
+```
+
+Run front in a 2nd terminal
+```bash
+cd frontend
+npm run dev
 ```
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
 /*===========INIT===========*/
 const express = require('express');
 const mysql = require('mysql2');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const hostname = '127.0.0.1';
+const hostname = process.env.HOSTNAME;
 const port = 3000;
-
-/*===========WEB PAGES===========*/
-app.use(express.static(path.join(__dirname, 'public')));
 
 /*===========DB CONNECTION===========*/
 const db = mysql.createConnection({
