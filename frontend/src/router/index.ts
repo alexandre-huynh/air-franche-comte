473ac6a1389c3a4d9a7eebdb1ff9a201bb638484
HUYNH Alexandre <huynh@et.esiea.fr>
2025-07-17 13:37:33 -0400

    Removed unused dashboard reference

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
-import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import Profile from '../views/Profile.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
-  { path: '/dashboard', component: Dashboard },
   { path: '/profile', component: Profile },
   { path: '/login', component: Login },
   { path: '/gallery', name: 'gallery', component: () => import('@/views/Gallery.vue') },
